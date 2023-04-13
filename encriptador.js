89ade0640759ee00bb0d36cf1d89ea96d928181d
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2023-04-13 04:37:09 -0500

    encrypt/decrypt-app

---
+++ encriptador.js
@@ -0,0 +1,76 @@
+const text = document.getElementById("text");
+const output = document.getElementById("output");
+const encrypt = document.getElementById("encrypt");
+const decrypt = document.getElementById("decrypt");
+const copy = document.getElementById("copy");
+const reset = document.getElementById("reset");
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+//Object with the letters to be replaced
+const letters = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+};
+
+//Function to encrypt the text
+
+const encryptText = (text) => {
+  let encrypt = "";
+  for (let i = 0; i < text.length; i++) {
+    const letter = text[i].toLowerCase();
+    encrypt += letters[letter] || letter;
+  }
+  return encrypt;
+};
+
+//Function to decrypt the text
+
+const decryptText = () => {
+  const textEncrypt = text.value
+    .replaceAll(/enter/gi, "e")
+    .replaceAll(/imes/gi, "i")
+    .replaceAll(/ai/gi, "a")
+    .replaceAll(/ober/gi, "o")
+    .replaceAll(/ufat/gi, "u")
+    .toLowerCase();
+  return textEncrypt;
+};
+
+const lastLetter = /[a-z]/;
+
+text.addEventListener("input", (e) => {
+  e.target.value = text.value.toLowerCase();
+});
+
+output.disabled = true;
+
+encrypt.addEventListener("click", (e) => {
+  e.preventDefault();
+  const encryptValue = encryptText(text.value);
+  output.value = encryptValue;
+});
+
+decrypt.addEventListener("click", (e) => {
+  e.preventDefault();
+  const decryptValue = decryptText(text.value);
+  output.value = decryptValue;
+});
+
+copy.addEventListener("click", (test) => {
+  test = output.innerHTML = output.value;
+  navigator.clipboard.writeText(test);
+  text.value = "";
+});
+
+reset.addEventListener("click", () => {
+  text.value = "";
+  output.value = "";
+});
